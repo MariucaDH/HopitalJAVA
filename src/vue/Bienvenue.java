a9571b9e559801bf52abd3dfb3133ac66df2cc28
Mariuca de Hillerin <mdehillerin@gmail.com>
2018-04-17 17:15:08 +0200

    ActionPerformed (boutons)

--- src/vue/Bienvenue.java
+++ src/vue/Bienvenue.java
@@ -141,11 +141,11 @@ public class Bienvenue extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+    new Reporting().setVisible(true);             // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+     new Ajout().setVisible(true);            // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
